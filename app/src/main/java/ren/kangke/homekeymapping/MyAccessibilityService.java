d804743833895da1a741189bbff9423ba421d02e
renkangke <renkangke@hujiang.com>
2017-08-23 13:18:21 +0800

    modify: press 300ms home button will trigger home action.

--- app/src/main/java/ren/kangke/homekeymapping/MyAccessibilityService.java
+++ app/src/main/java/ren/kangke/homekeymapping/MyAccessibilityService.java
@@ -1,21 +1,32 @@
 package ren.kangke.homekeymapping;
 
 import android.accessibilityservice.AccessibilityService;
-import android.os.Build;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
 
+import java.lang.ref.WeakReference;
+
 /**
+ * The accessibility service for change home key to be back.
  * Created by renkangke .
  */
 
 public class MyAccessibilityService extends AccessibilityService {
     private static final String TAG = "MyAccessibilityService";
-    public static final int INTERVAL_TIME = 300;
 
-    boolean mIsFirstHome = false;
-    long mFirstHomeEvent = 0;
+    /**
+     * The interval time of home action.
+     */
+    public static final int INTERVAL_TIME = 300;
+    public static final int HANDLER_HOME_DELAY_TIME_WHAT = 100001;
+
+    private boolean mIsFirstHomeAction = false;
+    private long mFirstHomeEventTime = 0;
+
+    final Handler mAccessibilityHandler = new AccessibilityHandler(this);
 
     @Override
     public void onCreate() {
@@ -33,14 +44,16 @@ public class MyAccessibilityService extends AccessibilityService {
         int key = event.getKeyCode();
         switch (key) {
             case KeyEvent.KEYCODE_HOME:
-                mIsFirstHome = !mIsFirstHome;
-                if (mIsFirstHome) {
-                    mFirstHomeEvent = System.currentTimeMillis();
+                mIsFirstHomeAction = !mIsFirstHomeAction;
+                if (mIsFirstHomeAction) {
+                    mFirstHomeEventTime = System.currentTimeMillis();
+                    mAccessibilityHandler.sendEmptyMessageDelayed(HANDLER_HOME_DELAY_TIME_WHAT, INTERVAL_TIME);
                 } else {
-                    long interval = System.currentTimeMillis() - mFirstHomeEvent;
+                    long interval = System.currentTimeMillis() - mFirstHomeEventTime;
                     if (interval > INTERVAL_TIME) {
                         return super.onKeyEvent(event);
                     } else {
+                        mAccessibilityHandler.removeMessages(HANDLER_HOME_DELAY_TIME_WHAT);
                         performGlobalAction(AccessibilityService.GLOBAL_ACTION_BACK);
                         return true;
                     }
@@ -51,12 +64,31 @@ public class MyAccessibilityService extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.i(TAG, "action" + event.getAction() + "," + event.getPackageName() + ",type:" + event.getEventType() + ", des:" + event.getContentDescription());
     }
 
     @Override
     public void onInterrupt() {
+    }
 
+    private static class AccessibilityHandler extends Handler {
+        WeakReference<MyAccessibilityService> mServiceWeakReference;
+
+        AccessibilityHandler(MyAccessibilityService serviceWeakReference) {
+            mServiceWeakReference = new WeakReference<>(serviceWeakReference);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case HANDLER_HOME_DELAY_TIME_WHAT:
+                    if (mServiceWeakReference != null && mServiceWeakReference.get() != null) {
+                        mServiceWeakReference.get().performGlobalAction(AccessibilityService.GLOBAL_ACTION_HOME);
+                    }
+                    removeMessages(HANDLER_HOME_DELAY_TIME_WHAT);
+                    break;
+            }
+        }
     }
 
 }
