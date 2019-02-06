38f2e24f1edb49c6cd78953994e114f878b37a6d
rarnu <hexiaojie@hujiang.com>
2019-02-07 00:06:59 +0800

    upgrade: to ktfunctional 0.8.0

--- app/src/main/kotlin/ren/kangke/homekeymapping/AboutActivity.kt
+++ app/src/main/kotlin/ren/kangke/homekeymapping/AboutActivity.kt
@@ -1,25 +1,23 @@
+@file:Suppress("DEPRECATION")
+
 package ren.kangke.homekeymapping
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.os.Bundle
-import android.view.MenuItem
+import com.rarnu.kt.android.BackActivity
 import com.rarnu.kt.android.resStr
-import com.rarnu.kt.android.showActionBack
 import kotlinx.android.synthetic.main.fragment_about.*
 
 /**
  * Created by rarnu on 9/4/17.
  */
-class AboutActivity : Activity() {
+class AboutActivity : BackActivity() {
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_about)
-        actionBar.title = resStr(R.string.app_about)
-        showActionBack()
-
+        actionBar?.title = resStr(R.string.app_about)
         btnDonateAlipay.setOnClickListener { DonateUtil.donateAlipay(this) }
 
         try {
@@ -28,13 +26,6 @@ class AboutActivity : Activity() {
         } catch (e: Exception) {
 
         }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            android.R.id.home -> finish()
-        }
-        return true
     }
 
 }
